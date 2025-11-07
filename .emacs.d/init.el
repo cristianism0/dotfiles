6bf5287a49acdfe6358701cd2500a296287e6960
cristianism0 <csg2099@gmail.com>
2025-11-07 20:21:16 -0300

    remove start up message

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -3,6 +3,10 @@
   (expand-file-name
     "config.org"
     user-emacs-directory))
+
+(setq inhibit-startup-message t)
+
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
