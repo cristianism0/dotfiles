2624b15441b1fe32468291bde6a2b9c27266e876
cristianism0 <csg2099@gmail.com>
2025-11-07 20:17:37 -0300

    full add

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -8,7 +8,13 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(evil evil-collection rainbow-mode)))
+ '(package-selected-packages
+   '(consult diminish doom-modeline eglot-booster magit markdown
+             markdown-mode neotree ordeless orderless
+             rainbow-delimiters rainbow-mode vertico vterm))
+ '(package-vc-selected-packages
+   '((eglot-booster :url "https://github.com/jdtsmith/eglot-booster"
+                    :branch "main"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
