8d63df3e1baf9b84f217f1a602f2d814cf06cd0b
cristianism0 <csg2099@gmail.com>
2026-02-18 12:08:16 -0300

    chore: add fedora icon on waybar

--- aliases.zsh
+++ aliases.zsh
@@ -14,24 +14,4 @@ alias synt="syncthing"
 alias vim="nvim"
 alias shutdown="shutdown now"
 
-rustrun() {
-    local arquivo="$1"
-    local destino="$2"
-    shift 2
-    mkdir -p "$destino"
-    
-    local binario="${arquivo%.rs}"
-    
-    rustc "$arquivo" -o "$destino/$binario" "$@" && "./$destino/$binario"
-}
 
-c-run() {
-    local arquivo="$1"
-    local destino="$2"
-    shift 2
-    mkdir -p "$destino"
-
-    local binario="${arquivo%.c}"
-    
-    gcc "$arquivo" -o "$destino/$binario" "$@" && "./$destino/$binario"
-}
