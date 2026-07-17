82a858bbcb852d8d8f349081811890029fae3b49
cristianism0 <csg2099@gmail.com>
2026-07-17 14:05:43 -0300

    fix: niri display

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,20 +1,28 @@
 return {
+    --from the primeagen
     "neovim/nvim-lspconfig",
     dependencies = {
+        "stevearc/conform.nvim",
         "williamboman/mason.nvim",
         "williamboman/mason-lspconfig.nvim",
+        "hrsh7th/cmp-nvim-lsp",
+        "hrsh7th/cmp-buffer",
+        "hrsh7th/cmp-path",
+        "hrsh7th/cmp-cmdline",
+        "hrsh7th/nvim-cmp",
+        "L3MON4D3/LuaSnip",
+        "saadparwaiz1/cmp_luasnip",
         "j-hui/fidget.nvim",
     },
 
     config = function()
-        vim.api.nvim_create_autocmd("LspAttach", {
-            callback = function(args)
-                local client = vim.lsp.get_client_by_id(args.data.client_id)
-                if client and client:supports_method("textDocument/completion") then
-                    vim.lsp.completion.enable(true, client.id)
-                end
-            end,
-        })
+        local cmp = require('cmp')
+        local cmp_lsp = require("cmp_nvim_lsp")
+        local capabilities = vim.tbl_deep_extend(
+            "force",
+            {},
+            vim.lsp.protocol.make_client_capabilities(),
+            cmp_lsp.default_capabilities())
 
         require("fidget").setup({})
         require("mason").setup()
@@ -22,38 +30,78 @@ return {
             ensure_installed = {
                 "lua_ls",
                 "rust_analyzer",
+                "gopls",
                 "clangd",
                 "ruff",
                 "basedpyright",
-                "gopls"
             },
             handlers = {
                 function(server_name)
-                    require("lspconfig")[server_name].setup({})
+                    require("lspconfig")[server_name].setup {
+                        capabilities = capabilities
+                    }
                 end,
-
-                ["lua_ls"] = function()
-                    require("lspconfig").lua_ls.setup({
-                        settings = {
-                            Lua = {
-                                diagnostics = {
-                                    globals = { "vim", "it", "describe", "before_each", "after_each" },
-                                }
-                            }
-                        }
-                    })
-                end,
-
                 ["clangd"] = function()
                     require("lspconfig").clangd.setup({
                         -- cmd = { "clangd", "--all-scopes-completion" }
                     })
                 end,
+
+                ["lua_ls"] = function()
+                    local lspconfig = require("lspconfig")
+
+                    lspconfig.lua_ls.setup {
+                        capabilities = capabilities,
+                        settings = {
+                            Lua = {
+                                runtime = {
+                                    version = 'LuaJIT',
+                                },
+                                diagnostics = {
+                                    globals = { 'vim' },
+                                },
+                                workspace = {
+                                    library = vim.api.nvim_get_runtime_file("", true),
+                                    checkThirdParty = false,
+                                },
+                                format = {
+                                    enable = true,
+                                    defaultConfig = {
+                                        indent_style = "space",
+                                        indent_size = "2",
+                                    }
+                                },
+                            }
+                        }
+                    }
+                end,
             }
         })
 
+        local cmp_select = { behavior = cmp.SelectBehavior.Select }
+
+        cmp.setup({
+            snippet = {
+                expand = function(args)
+                    require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+                end,
+            },
+            mapping = cmp.mapping.preset.insert({
+                ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+                ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+                ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+                ["<C-Space>"] = cmp.mapping.complete(),
+            }),
+            sources = cmp.config.sources({
+                { name = 'nvim_lsp' },
+                { name = 'luasnip' },
+            }, {
+                { name = 'buffer' },
+            })
+        })
+
         vim.diagnostic.config({
-            update_in_insert = true,
+            -- update_in_insert = true,
             float = {
                 focusable = false,
                 style = "minimal",
@@ -63,14 +111,5 @@ return {
                 prefix = "",
             },
         })
-
-        vim.keymap.set('i', '<C-Space>', '<C-x><C-o>', { desc = "Open Autocomplete" })
-        vim.keymap.set('i', '<C-y>', function()
-            if vim.fn.pumvisible() ~= 0 then
-                return '<C-y>'
-            else
-                return '<C-y>'
-            end
-        end, { expr = true })
     end
 }
