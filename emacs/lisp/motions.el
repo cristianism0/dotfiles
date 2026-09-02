0d2b853de6e00098cac24b38c97d95f82825db0f
cristianism0 <csg2099@gmail.com>
2026-09-02 19:19:48 -0300

    chore(checkdoc): some checkdoc details

--- emacs/lisp/motions.el
+++ emacs/lisp/motions.el
@@ -1,4 +1,9 @@
-;;; Vertico
+;;; motions.el --- Manage keybing and motions -*- lexical-binding: t; no-byte-compile: t -*-
+;;; Commentary:
+;; This file is to manage the motions like: Evil, define keybindings and use packages to help the motions and Dired.
+
+;;; Code:
+;; Vertico
 ;; Vertical use of M-x
 (use-package vertico
       :ensure t
@@ -7,7 +12,7 @@
       :init
       (vertico-mode))
 
-;;; Marginalia
+;; Marginalia
 ;; Content on the margin
 (use-package marginalia
     :after vertico
@@ -16,7 +21,7 @@
     :init
     (marginalia-mode))
 
-;;; Orderless
+;; Orderless
 ;; Fuzzy search
 (use-package orderless
   :ensure t
@@ -24,5 +29,86 @@
   (completion-styles '(orderless basic))
   (completion-category-overrides '((file (styles basic partial-completion)))))
 
-;;; Mappings
+;; Which key
+(use-package which-key
+  :ensure t
+  :init (which-key-mode)
+  :custom
+  (which-key-idle-delay 1.0))
 
+;; Consul
+(use-package consult
+  :bind (
+         ("C-s" . consult-line)
+         ("C-x b" . consult-buffer)
+         ("M-y" . consult-yank-pop)
+         ("M-g g" . consult-goto-line)
+         ("M-g i" . consult-imenu)
+         ("C-c r" . consult-ripgrep))
+  :init
+  (setq register-preview-delay 0.5
+        register-preview-function #'consult-register-format)
+  :config
+  (consult-customize
+   consult-theme :preview-key '(:debounce 0.2 any)
+   consult-ripgrep consult-line :preview-key '(:debounce 0.1 any)))
+
+;; Undo Fu
+(use-package undo-fu
+  :ensure t)
+
+;; Evil Mode (Vim Keybindings)
+(use-package evil
+  :init
+  (setq evil-want-integration t)
+  (setq evil-want-keybinding nil)
+  (setq evil-want-C-u-scroll t)
+  (setq evil-want-C-i-jump t)
+  (setq evil-want-fine-undo t)
+  (setq evil-undo-system 'undo-redo)
+
+  :config
+  (evil-mode 1)
+  (setq evil-move-cursor-back nil))
+  ()
+
+(use-package evil-collection
+  :after evil
+  :config
+  (evil-collection-init))
+
+(use-package general
+  :after evil
+  :config
+  (general-evil-setup t))
+
+  (general-create-definer my-doom-leader-def
+    :states '(normal visual motion emacs)
+    :keymaps 'override
+    :prefix "SPC"
+    :global-prefix "M-SPC")
+
+  (my-doom-leader-def
+    "."   '(find-file :which-key "Find file")
+    ","   '(switch-to-buffer :which-key "Switch buffer")
+    "SPC" '(execute-extended-command :which-key "M-x")
+
+    "b"   '(:ignore t :which-key "buffer")
+    "b b" '(switch-to-buffer :which-key "Switch buffer")
+    "b d" '(kill-current-buffer :which-key "Kill buffer")
+    "b n" '(next-buffer :which-key "Next buffer")
+    "b p" '(previous-buffer :which-key "Previous buffer")
+
+    "f"   '(:ignore t :which-key "file")
+    "f f" '(find-file :which-key "Find file")
+    "f s" '(save-buffer :which-key "Save file")
+    "f r" '(recentf-open-files :which-key "Recent files")
+
+    "w"   '(:ignore t :which-key "window")
+    "w s" '(split-window-below :which-key "Split horizontal")
+    "w v" '(split-window-right :which-key "Split vertical")
+    "w w" '(other-window :which-key "Other window")
+    "w d" '(delete-window :which-key "Delete window"))
+
+(provide 'motions)
+;;; motions.el ends here;
--- emacs/lisp/ui-basics.el
+++ emacs/lisp/ui-basics.el
@@ -1,11 +1,15 @@
-;;; UI features:
+;;; ui-basics.el --- UI features -*- lexical-binding: t; no-byte-compile: t -*-
+
+;;; Commentary:
+;; This file is to define basic UI configratuions, since icos hidden to fonts choice.
+
+;;; Code:
+;; UI Icons:
 (tool-bar-mode   -1)	                   ; Hide tools bar
 (menu-bar-mode   -1)	                   ; Hide menu bar
 (scroll-bar-mode -1)	                   ; Hide scroll bar
 (set-fringe-mode  5)	                   ; Frame Edges (5px)
 (save-place-mode +1)	                   ; Save cursor position
-(recentf-mode    +1)	                   ; Recent Files
-(savehist-mode   +1)	                   ; Enable history saving
 (delete-selection-mode t)                  ; Delete a selected line if write on it
 (global-visual-line-mode t)                ; Line break
 (setq-default indent-tabs-mode t) 	   ; Space indentations
@@ -16,8 +20,36 @@
 (setq frame-resize-pixelwise t)            ; Resize on WM/Tillings
 (display-battery-mode 0)                   ; Show the battery in the modeline
 (setq-default fill-column 80)              ; Fix a line on 80's colummn
+(global-hl-line-mode 1)                    ; Highlight the cursor line
+(modify-all-frames-parameters '((internal-border-width . 12)))
 
 ;;; Fonts:
-(set-face-attribute 'default nil :family "Iosevka Nerd Font" :height 150)
+(set-face-attribute 'default nil
+                    :family "Fira Code Nerd Font"
+                    :height 140
+                    :weight 'normal)
+(set-face-attribute 'variable-pitch nil
+                    :family "Inter"
+                    :height 150)
+(set-face-attribute 'fixed-pitch-serif nil
+                    :family "Alegreya"
+                    :height 160)
+(set-fontset-font t 'symbol (font-spec :family "JuliaMono") nil 'append)
 
 (add-hook 'before-save-hook #'delete-trailing-whitespace)
+
+(use-package hl-todo
+  :ensure t
+  :hook (prog-mode . hl-todo-mode)
+  :config
+  (setq hl-todo-highlight-punctuation ":"
+        hl-todo-keyword-faces
+        '(("TODO"       . (warning bold))
+          ("FIXME"      . (error bold))
+          ("HACK"       . (font-lock-constant-face bold))
+          ("REVIEW"     . (font-lock-keyword-face bold))
+          ("NOTE"       . (success bold))
+          ("DEPRECATED" . (font-lock-doc-face bold)))))
+
+(provide 'ui-basics)
+;;; ui-basics.el ends here
